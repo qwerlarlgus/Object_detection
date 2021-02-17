d099716f5602c0bcfbf6bc0361ffbceee04180fa
KiHyun-KIM <75463182+qwerlarlgus@users.noreply.github.com>
2021-02-17 14:25:40 +0900

    Project ver 0.1

---
+++ prj1/entry_exit.py
@@ -0,0 +1,9 @@
+entry = 10   # Occupied space
+exit = 5  # Free space
+
+# 
+
+print('Entry:', entry)
+print('Exit:', exit)
+
+# return occupied free_space
---
+++ prj1/free_space.py
@@ -0,0 +1,9 @@
+occupied = 10   # Occupied space
+free_space = 5  # Free space
+
+# 
+
+print('Occupied:', occupied)
+print('Free Space:', free_space)
+
+# return occupied free_space
